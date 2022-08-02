1da2ea937974ee227b39c8ab3e88aa5672cb3c74
poyrazemun <ofatih-poyraz@hotmail.com>
2022-08-02 13:50:13 +0200

    @SerenityTest added

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,41 @@
 package eu8.spartan.admin;
 
-public class SpartanAdminGetTest  {
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SerenityTest
+public class SpartanAdminGetTest {
+    @BeforeAll
+    public static void init() {
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://3.83.68.127:7000";
+    }
+
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+    }
+
+
+
+
+
+
 }
